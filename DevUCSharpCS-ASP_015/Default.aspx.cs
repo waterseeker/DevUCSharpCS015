708cba0cc8a915d1f21db4cec2df020558b35240
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-22 09:23:23 -0500

    added a different example of using TimeSpan and DateTime

--- DevUCSharpCS-ASP_015/Default.aspx.cs
+++ DevUCSharpCS-ASP_015/Default.aspx.cs
@@ -17,10 +17,19 @@ namespace DevUCSharpCS_ASP_015
         protected void Button1_Click(object sender, EventArgs e)
         {
             // Days.Hour.Minutes:Seconds.Milliseconds
+            // http://is.gd/timespan
             //TimeSpan myTimeSpan = TimeSpan.Parse("7.7.7:7.7");
 
-            DateTime myBirthday = DateTime.Parse("12/7/1969");
-            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            //show elapsed time between a given date and DateTime.Now
+            //DateTime myBirthday = DateTime.Parse("12/7/1969");
+            //TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            //resultLabel.Text = myAge.TotalDays.ToString();
+
+            DateTime myDate = DateTime.Parse("1/1/0001");
+            TimeSpan myTime = DateTime.Now.Subtract(myDate);
+
+            resultLabel.Text = "There have been " + myTime.TotalDays.ToString() + " days since Jan 1, 0001.";
         }
     }
 }
